dade249dd784dbdc232bf86a9ec4e44dfac45ec3
Christo Papadopoulo <christo.papadopoulo@iris.co.uk>
2023-03-14 11:56:12 +0000

    messing around with mongodb

--- src/HotelMap/Hotels/Parsers/ImportFeed.php
+++ src/HotelMap/Hotels/Parsers/ImportFeed.php
@@ -5,7 +5,7 @@ use App\HotelMap\Hotels\Parsers\Feed;
 use App\HotelMap\Hotels\Parsers\FeedInterface;
 use MongoDB;
 
-class JsonFeed extends Feed implements FeedInterface
+class ImportFeed extends Feed implements FeedInterface
 {
     /**
      * Parse data into a series of rooms and a series of rates associated with each room.
@@ -17,7 +17,7 @@ class JsonFeed extends Feed implements FeedInterface
         // The data is in JSON format
         // Using the data, parse into a series of rooms with a series of rates
         // $this->addRoom("double room", [123,654,232]);
-    //    $data = json_decode($this->data, true);
+        //    $data = json_decode($this->data, true);
 
 // Recursive function to get all keys and values from a nested array
         function get_array_keys_and_values_recursive($array, &$result, $parent_key = null) {
@@ -38,19 +38,19 @@ class JsonFeed extends Feed implements FeedInterface
         get_array_keys_and_values_recursive($data, $result);
 
 // Output array of keys and values
- //       print_r($result);
+        //       print_r($result);
 
-     //   // Connect to MongoDB
+        //   // Connect to MongoDB
         $mongoClient = new MongoDB\Client('mongodb://mongo:27017');
 
 // Select database and collection
         $collection = $mongoClient->myDatabase->myCollection;
 
 // Read JSON data from a file or HTTP request
-   //     $jsonData = file_get_contents('path/to/json/file.json');
+        //     $jsonData = file_get_contents('path/to/json/file.json');
 
 // Decode JSON data into an associative array
-  //      $data = json_decode($jsonData, true);
+        //      $data = json_decode($jsonData, true);
 
 // Insert JSON data into MongoDB
         $result = $collection->insertMany($data);
@@ -58,7 +58,7 @@ class JsonFeed extends Feed implements FeedInterface
 
         //[['$match' => ['rooms.room_name' => 'Apartment, 1 Bedroom']], ['$unwind' => '$rooms'], ['$match' => ['rooms.room_name' => 'Apartment, 1 Bedroom']], ['$unwind' => '$rooms.rates'], ['$sort' => ['rooms.rates.occupancy_pricing.2-9,9.totals.inclusive.billable_currency.value' => 1]]]
 
-echo 'dsfsdf';
+        echo 'dsfsdf';
         foreach ($data as $property) {
             echo 'property:' . $property['property_id']  . '<br />';
 
@@ -69,7 +69,7 @@ echo 'dsfsdf';
                     echo 'rate:' . $rate['id'] . '<br />';
                 }
             }
-          //  print_r($record);
+            //  print_r($record);
 
         }
         die();
